27adc27839769efd6e653dfecf83cc76227b036f
Łukasz Godlewski <lgodlewski@itsg.com.pl>
2025-09-24 08:19:54 +0200

    update nvim

---
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      json = { "prettierd" },
+      css = { "prettierd" },
+      html = { "prettierd" },
+      markdown = { "prettierd" },
+    },
+  },
+}
