c2d47a0b5c1b472d19cb2e8a0b72300b7ff07a6d
Łukasz Godlewski <lgodlewski@itsg.com.pl>
2025-12-12 12:13:26 +0100

    cachyos install script

---
+++ cachyos/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install Dank Linux
+curl -fsSL https://install.danklinux.com | sh
+
+# Several utility packages
+paru -S --noconfirm github-cli
+paru -S --noconfirm chezmoi
+paru -S --noconfirm zoxide
+paru -S --noconfirm neovim
+paru -S --noconfirm lazygit
+paru -S --noconfirm zen-browser
+paru -S --noconfirm keepassxc
+# paru -S --noconfirm gdm
+paru -S --noconfirm dropbox
+
+paru -S --noconfirm xremap # to make single META button work
+
+paru -S --noconfirm elephant
+paru -S --noconfirm walker
+elephant service enable
+
+chezmoi init luski --branch cachyos --apply
