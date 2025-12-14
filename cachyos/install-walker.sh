5927443e87df71c8ac39bbff3014f4f3d4fb3cd8
Łukasz Godlewski <lgodlewski1987@gmail.com>
2025-12-14 20:18:23 +0100

    update elephant install

--- cachyos/install-walker.sh
+++ cachyos/install-walker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-paru -S --noconfirm walker elephant elephant-desktopapplications elephant-websearch elephant-todo
+paru -S --noconfirm walker elephant elephant-all
 chezmoi apply ~/.config/systemd/user
 systemctl --user enable elephant --now
 systemctl --user enable walker --now
