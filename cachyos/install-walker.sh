3d37bb243a19912978ebb93d8e7011f128404800
Łukasz Godlewski <lgodlewski1987@gmail.com>
2025-12-15 20:10:35 +0100

    walker fix

--- cachyos/install-walker.sh
+++ cachyos/install-walker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-paru -S --noconfirm walker elephant elephant-all
+paru -S --noconfirm walker elephant-all
 chezmoi apply ~/.config/systemd/user
 systemctl --user enable elephant --now
 systemctl --user enable walker --now
