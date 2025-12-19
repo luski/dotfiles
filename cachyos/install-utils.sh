30f7b89ec1b0f0acd4dcae23acaa627261946cf3
Łukasz Godlewski <lgodlewski1987@gmail.com>
2025-12-19 21:32:35 +0100

    add tmux

--- cachyos/install-utils.sh
+++ cachyos/install-utils.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-paru -S --needed --noconfirm github-cli
-paru -S --needed --noconfirm chezmoi
-paru -S --needed --noconfirm zoxide
-paru -S --needed --noconfirm neovim
-paru -S --needed --noconfirm lazygit
-paru -S --needed --noconfirm zen-browser
-paru -S --needed --noconfirm libappindicator dropbox
-paru -S --needed --noconfirm fnm
-paru -S --needed --noconfirm obsidian
-paru -S --needed --noconfirm qt5-wayland keepassxc
-paru -S --needed --noconfirm uwsm
-curl -fsSL https://tailscale.com/install.sh | sh
+utils=(
+	github-cli
+	chezmoi
+	zoxide
+	neovim
+	lazygit
+	zen-browser
+	libappindicator dropbox
+	fnm
+	obsidian
+	qt5-wayland keepassxc
+	uwsm
+	try-cli
+)
 
 # niri specific
-packages=(
+niriSpecific=(
 	nwg-look
 	adw-gtk-theme
 	adwaita-color-schemes
@@ -25,4 +27,7 @@ packages=(
 	dialect
 )
 
-paru -S --needed --noconfirm "${packages[@]}"
+paru -S --needed noconfirm "${utils[@]}"
+paru -S --needed --noconfirm "${niriSpecific[@]}"
+
+curl -fsSL https://tailscale.com/install.sh | sh
